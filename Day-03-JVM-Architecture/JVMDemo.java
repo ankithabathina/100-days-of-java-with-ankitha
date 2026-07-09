88b348244ae1694cac4f574616164ba69b39def8
AnkithaBathina. <bathinaankitha@gmail.com>
2026-07-09 10:27:00 +0530

    Create JVMDemo.java

---
+++ Day-03-JVM-Architecture/JVMDemo.java
@@ -0,0 +1,19 @@
+public class JVMDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== JVM Execution Flow =====");
+
+        System.out.println("Step 1 : Write Java Source Code (.java)");
+        System.out.println("Step 2 : Compile using javac");
+        System.out.println("Step 3 : Generate Bytecode (.class)");
+        System.out.println("Step 4 : Class Loader loads the class");
+        System.out.println("Step 5 : Bytecode Verifier checks the code");
+        System.out.println("Step 6 : Execution Engine executes the bytecode");
+        System.out.println("Step 7 : JIT Compiler improves performance");
+        System.out.println("Step 8 : Garbage Collector removes unused objects");
+        System.out.println("Step 9 : Output is displayed");
+
+        System.out.println("\nJVM allows Java programs to run on any platform.");
+    }
+}
